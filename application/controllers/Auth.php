ed3c09c298a9ed0f19d553d275fa2431818defea
hilman-firdd <hilmanfirdaus48@gmail.com>
2019-08-26 19:24:41 +0700

    selamat mengerjakan

---
+++ application/controllers/Auth.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+    public function index()
+    {
+        $this->load->view('templates/auth_header');
+        $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
+    }
+    public function register()
+    {
+        $this->load->view('templates/auth_header');
+        $this->load->view('auth/register');
+        $this->load->view('templates/auth_footer');
+    }
+}
